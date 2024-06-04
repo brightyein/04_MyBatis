62dc008c997e763f446e2fb024e53c14f872afb0
brightyein <129862727+brightyein@users.noreply.github.com>
2024-06-04 17:53:51 +0900

    new commit

---
+++ chap03-crud/src/main/java/com/ohgiraffers/section01/xmlconfig/model/MenuDAO.java
@@ -0,0 +1,34 @@
+package com.ohgiraffers.section01.xmlconfig.model;
+
+import com.ohgiraffers.dto.MenuDTO;
+import org.apache.ibatis.session.SqlSession;
+
+import java.util.List;
+
+public class MenuDAO {
+
+    public List<MenuDTO> selectAllMenu(SqlSession sqlSession){
+
+        return sqlSession.selectList("MenuMapper.selectAllMenu");
+    }
+
+    public MenuDTO selectMenuByCode(SqlSession sqlSession, int code) {
+
+        return sqlSession.selectOne("MenuMapper.selectMenuByCode", code);
+    }
+
+    public int insertMenu(SqlSession sqlSession, MenuDTO menu) {
+
+        return sqlSession.insert("MenuMapper.insertMenu", menu);
+    }
+
+    public int updateMenu(SqlSession sqlSession, MenuDTO menu) {
+
+        return sqlSession.update("MenuMapper.updateMenu", menu);
+    }
+
+    public int deleteCode(SqlSession sqlSession, int code) {
+
+        return sqlSession.delete("MenuMapper.deleteMenu", code);
+    }
+}
